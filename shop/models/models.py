00d186121d88db36ab7affa253991eac1da72daf
yurii <yurii.feshaka@gmail.com>
2021-10-15 15:20:58 +0200

    change tree view

--- shop/models/models.py
+++ shop/models/models.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+import random
 
+class shop(models.Model):
+    _name = 'shop.shop'
+    _description = 'shop.shop'
 
-# class shop(models.Model):
-#     _name = 'shop.shop'
-#     _description = 'shop.shop'
+    def _generate_number(self):
+        return str(random.randint(0000000000000,9999999999999))
+    number = fields.Char(default=_generate_number,string = "Номер", required=True, readonly=True)
+    _sql_constraints = [('number_unique', 'unique(number)', 'Згенероване число не унікальне, повтори спробу!')]
+    availability = fields.Selection([('availabe1','На складі'),
+                               ('availabe2', 'Під замовлення')],
+                                string = "Наявність")
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
