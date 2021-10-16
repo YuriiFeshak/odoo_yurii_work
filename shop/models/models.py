05ca60016b2256614d12edc78a6b2c4a3e7aefb2
yurii <yurii.feshaka@gmail.com>
2021-10-16 13:34:48 +0200

    перейменував модулі, зробив переклад

--- shop/models/models.py
+++ shop/models/models.py
@@ -8,21 +8,21 @@ class shop(models.Model):
     _name = 'shop.shop'
     _description = 'shop.shop'
 
-    # генератор випадкових 13 значних чисел, правда незнаю чи унікальних
+    # генератор випадкових 13 значних чисел, правда не унікальних
     def _generate_number(self):
         range_start = 10**(13-1)
         range_end = (10**13)-1
         return randint(range_start, range_end)
     # поля
-    number = fields.Char(default=_generate_number,string = "Номер", required=True, readonly=True)
+    number = fields.Char(default=_generate_number,string = "Number", required=True, readonly=True)
     # перевірка якщо не унікально згенероване число
-    _sql_constraints = [('number_unique', 'unique(number)', 'Згенероване число не унікальне, повтори спробу!')]
-    availability = fields.Selection([('availabe1','На складі'),
-                               ('availabe2', 'Під замовлення')],
-                                string = "Наявність")
+    _sql_constraints = [('number_unique', 'unique(number)', 'The generate number must be unique, try again!')]
+    availability = fields.Selection([('availabe_1','In stock'),
+                               ('availabe_2', 'Under the order')],
+                                string = "Availability")
 
 class category_product(models.Model):
     _name = 'category.product'
 
-    numb  = fields.Char(string="Назва")
-    numb1 = fields.Char(string="Опис")
+    numb = fields.Char(string="Name")
+    numb1 = fields.Char(string="Description")
