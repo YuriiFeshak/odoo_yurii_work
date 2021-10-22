7a2898844bf7846659759138bae3ba0a21c4a700
yurii <yurii.feshaka@gmail.com>
2021-10-22 15:30:09 +0200

    Добавив розрахунок полів

--- shop/models/models.py
+++ shop/models/models.py
@@ -26,16 +26,16 @@ class CategoryProducts(models.Model):
 
     name = fields.Char(string="Сategory products", )
     products_description = fields.Char(string="Description")
-    # products_id = fields.Many2one('products', string ="product store")
+
     # def default_get(self, fields):
         # res = super(CategoryProducts, self).default_get(fields)
         # res['name'] = 'Automotive products'
         # return res
 
-class Products(models.Model):
+class ProductsStore(models.Model):
     _name = 'products.store'
     # _inherit = 'category.products'
-    # products_id = fields.Many2one('category.products', string="product store")
+
     products_category_id = fields.Many2one('category.products',  string="Name products")
     name = fields.Char(string="Product name")
     availability_products = fields.Selection([('availabe_1','In stock'),
@@ -43,4 +43,14 @@ class Products(models.Model):
                                 string="Availability")
 
     description_text_id = fields.Text(string="Product descriptions")
+    stock_in = fields.Integer(string='Stock in', readonly=True)
+    stock_out = fields.Integer(string='Stock Out', readonly=True)
+    left_field = fields.Integer(string='Left', readonly=True, compute='_compute_left')
 
+    # Розрахунок поля Left
+    @api.depends('stock_in', 'stock_out')
+    def _compute_left(self):
+        if self.stock_in > self.stock_out:
+            self.left_field = self.stock_in - self.stock_out
+        else:
+            self.left_field = False
