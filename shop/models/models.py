9cef5e63cb58e0acad80fe2e4f0929cae60acce1
yurii <yurii.feshaka@gmail.com>
2021-10-29 16:02:08 +0200

    робоча можоивість в add in store добавляти нові значення

--- shop/models/models.py
+++ shop/models/models.py
@@ -23,6 +23,7 @@ class Shop(models.Model):
 
 class CategoryProducts(models.Model):
     _name = 'category.products'
+    _description = 'category products'
 
     name = fields.Char(string="Сategory products")
     products_description = fields.Char(string="Description")
@@ -35,6 +36,8 @@ class CategoryProducts(models.Model):
 
 class ProductsStore(models.Model):
     _name = 'products.store'
+    _description = 'products store'
+
 
 
     products_category_id = fields.Many2one('category.products', string="Products category")
@@ -45,13 +48,9 @@ class ProductsStore(models.Model):
                                 string="Availability")
     description_text_id = fields.Text(string="Product descriptions")
     stock_id = fields.Many2one('add.in.store')
-    stock_in = fields.Integer(string='Stock in',  related='stock_id.up_stock_in')
-    stock_out = fields.Integer(string='Stock Out')
+    stock_in = fields.Integer(string='Stock in', related='stock_id.up_stock_in')
+    stock_out = fields.Integer(string='Stock Out', related='stock_id.up_stock_out')
     left_field = fields.Integer(string='Left', readonly=True, compute='_compute_left')
-
-
-    # def write(self, value):
-    #     return super(ProductsStore, self).write(value)
 
     # Розрахунок поля Left
     @api.depends('stock_in', 'stock_out')
@@ -67,10 +66,13 @@ class ProductsStore(models.Model):
                 'type': 'ir.actions.act_window',
                 'res_model': 'add.in.store',
                 'view_mode': 'form',
-                'context': {'default_id': self.id},
+                'context': {'default_id': self.id, # потрібно щоб працювала можливість добавляти нові значення в полі
+                            'default_up_stock_in': self.stock_in,  #підзавантаження значення в полі
+                            'default_up_stock_out': self.stock_out,
+                            'default_up_left_field': self.left_field,
+                            },
                 'target': 'new'
         }
-
 
     def create_notification(self):
         return {
--- shop/wizard/add_in_store.py
+++ shop/wizard/add_in_store.py
@@ -7,22 +7,34 @@ class AddInStore(models.Model): # не може бути TransientModel тоді
     _name = "add.in.store"
     _description = "add in store"
 
-    # stock_id = fields.Many2one('products.store', string='Product Name')
     up_stock_in = fields.Integer(string='Stock in')
-    # up_stock_out = fields.Integer(string='Stock Out', related='name_id.stock_in')
-    # up_left_field = fields.Integer(string='Left(Not edited)')
+    up_stock_out = fields.Integer(string='Stock Out')
+    up_left_field = fields.Integer(string='Left')
 
     # добавлення даних в поля по кнопці add in store
     @api.model
-    def action_add_value(self, value):
+    def create(self, value):
         res = super(AddInStore, self).create(value)
         product = self.env['products.store'].search([('id', '=', self.env.context.get('default_id'))])
         if product:
             product.update({
-                'stock_id': self.id
+                'stock_id': res.id
             })
         return res
 
+    # вaріант 2 через кнопку action_add_value( якщо кнопка то тільки може бути один параметр self,без декоратора)
+    # def action_add_value(self):
+    #     values = {'up_stock_in': self.up_stock_in}
+    #
+    #     res = self.create(values)
+    #     product = self.env['products.store'].search([('id', '=', self.env.context.get('default_id'))])
+    #     if product:
+    #         product.update({
+    #             'stock_id': res.id
+    #         })
+    #     return res
+
+    # примітивне добавлення даних по кнопці add
     # def update_fields(self):
     #     # self.env['products.store'].search(self.context.get('default_id'))\
     #     self.env['products.store'].search([('id', '=', self.env.context.get('default_id'))]).update({
